51c778c0d27a25596568a32a84220da89e703821
Vipin <kirthane.vipin@gmail.com>
2020-03-04 12:26:16 -0500

    Added event handlers

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,37 +1,60 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
+  constructor() {
+    super();
+    console.log(this);
+  }
+
   state = {
     count: 0,
-    tags: ["tag1", "tag2", "tag3"]
+    tags: [1, 2, 3]
   };
 
-  formatCount = () => {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
-  };
+  renderTags() {
+    if (this.state.tags.length === 0)
+      return <p style={{ margin: 20 }}>There are no tags!</p>;
+
+    return (
+      <ul>
+        {this.state.tags.map(tag => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
+    );
+  }
 
   getBadgeClasses() {
-    let buttonShit = "btn btn-sm btn-";
-    buttonShit += this.state.count === 0 ? "warning" : "primary";
-    return buttonShit;
+    let classes = "badge badge-md badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
+  }
+
+  handleIncrement() {
+    console.log("Increment Clicked", this.state.count);
   }
 
   render() {
     return (
       <div>
         <span
-          style={{ fontSize: 20, margin: 10 }}
           className={this.getBadgeClasses()}
+          style={{ padding: 10, margin: 5 }}
         >
           {this.formatCount()}
         </span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
-        <ul>
-          {this.state.tags.map(tag => (
-            <li key={tag}>{tag}</li>
-          ))}
-        </ul>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-sm btn-secondary"
+        >
+          Increment
+        </button>
+        {this.renderTags()}
       </div>
     );
   }
